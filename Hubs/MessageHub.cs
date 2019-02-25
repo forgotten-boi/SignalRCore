62ff9d6504fca30aae235201556a5c42f453555e
Nish <nishjha4u@live.com>
2019-02-25 17:16:24 +0545

    two chat for individual, group and everyone from message hub

--- Hubs/MessageHub.cs
+++ Hubs/MessageHub.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.SignalR;
 using System.Threading.Tasks;
+using System;
+
 
 namespace AspNetCore.SignalR
 {
@@ -10,6 +12,39 @@ namespace AspNetCore.SignalR
             return Clients.All.SendAsync("RecieveMessage", message);
         }
 
-        
+        public Task SendMessageToCaller(string message)
+        {
+            return Clients.Caller.SendAsync("RecieveMessage", message);
+        }
+
+        public Task SendMessageToUser(string connectionId, string message)
+        {
+            return Clients.Client(connectionId).SendAsync("RecieveMessage", message);
+        }
+
+        public Task JoinGroup(string group)
+        {
+            return Groups.AddToGroupAsync(Context.ConnectionId, group);
+        }
+
+        public Task SendMessageToGroup(string group, string message)
+        {
+            return Clients.Group(group).SendAsync("RecieveMessage", message);
+        }
+
+
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("UserConnected", Context.ConnectionId);
+            await base.OnConnectedAsync();
+        }
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Clients.All.SendAsync("UserDisConnected", Context.ConnectionId);
+           
+            await base.OnDisconnectedAsync(exception);
+        }
+
+
     }
 }
